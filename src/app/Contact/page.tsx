67aabbb84655088311c984f8efd162a72c2b306f
Jean-Jacques Delegue <jean-jacques.delegue@epitech.eu>
2023-12-11 00:55:43 +0100

    remove grid

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,29 +1,20 @@
 "use client";
 
-import React, { useState, SyntheticEvent } from "react";
+import React from "react";
 
 import Layout from "@/layout";
 
 import Page from "@/components/Page";
 
-import { Grid, Card, CardContent } from "@mui/material";
-
-import FormContact from "./form";
+import { Grid } from "@mui/material";
 
 const Contact = () => {
 	return (
 		<Layout variant='main'>
 			<Page title='Contact'>
 				<Grid container spacing={2}>
-					<Grid item xs={12} lg={6}>
+					<Grid item xs={12} lg={12}>
 						<iframe src='CV_PHAN_Matthis.pdf' width='100%' height='850' />
-					</Grid>
-					<Grid item xs={12} lg={6}>
-						<Card>
-							<CardContent>
-								<FormContact />
-							</CardContent>
-						</Card>
 					</Grid>
 				</Grid>
 			</Page>
